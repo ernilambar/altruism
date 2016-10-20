60af0a81d15b40eab346b54e494e56c6c7cdda50
ernilambar <nilambar@outlook.com>
2016-10-20 14:05:57 +0545

    Remove angular

--- functions.php
+++ functions.php
@@ -22,20 +22,6 @@ function altruism_scripts() {
 
 	wp_enqueue_style( 'altruism-style', get_stylesheet_uri(), '', '2.0' );
 
-	wp_enqueue_script( 'angularjs', get_template_directory_uri() . '/third-party/angular/angular' . $min . '.js', '', '1.5.0', true );
-	wp_enqueue_script( 'angularjs-route', get_template_directory_uri() . '/third-party/angular/angular-route' . $min . '.js', '', '1.5.0', true );
-	wp_enqueue_script( 'angularjs-sanitize', get_template_directory_uri() . '/third-party/angular/angular-sanitize' . $min . '.js', '', '1.5.0', true );
-	wp_enqueue_script( 'altruism-custom', get_template_directory_uri() . '/js/app' . $min . '.js', array( 'angularjs' ), '1.0', true );
-
-	$base_url  = esc_url_raw( home_url() );
-	wp_localize_script( 'altruism-custom', 'altruismLocalized', array(
-		'root'      => esc_url_raw( rest_url() ),
-		'base_url'  => $base_url,
-		'nonce'     => wp_create_nonce( 'wp_rest' ),
-		'site_name' => get_bloginfo( 'name' ),
-		'partials'  => trailingslashit( get_template_directory_uri() ) . 'partials/',
-	) );
-
 }
 
 add_action( 'wp_enqueue_scripts', 'altruism_scripts' );
