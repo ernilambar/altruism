150209787ca3c496c5edb8ca49e018186630038e
ernilambar <nilambar@outlook.com>
2016-10-23 13:51:38 +0545

    Add menu walker

--- inc/hooks.php
+++ inc/hooks.php
@@ -41,3 +41,46 @@ if ( ! function_exists( 'altruism_add_templates' ) ) :
 endif;
 
 add_action( 'wp_footer', 'altruism_add_templates' );
+
+class Altruism_Menu_Walker extends Walker_Nav_Menu {
+
+	function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+
+		switch ( $item->object ) {
+			case 'page':
+				$item->url = home_url() . '/p/' . basename( get_permalink( $item->object_id ) );
+				break;
+
+			default:
+				break;
+		}
+		$indent = ( $depth > 0 ? str_repeat( "\t", $depth ) : '' );
+
+		// Passed classes.
+		$classes = empty( $item->classes ) ? array() : (array) $item->classes;
+		$class_names = esc_attr( implode( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item ) ) );
+
+		// Build HTML.
+		$output .= $indent . '<li id="nav-menu-item-'. $item->ID . '" class="' . ' ' . $class_names . '">';
+
+		// Link attributes.
+		$attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
+		$attributes .= ! empty( $item->target )     ? ' target="' . esc_attr( $item->target     ) .'"' : '';
+		$attributes .= ! empty( $item->xfn )        ? ' rel="'    . esc_attr( $item->xfn        ) .'"' : '';
+		$attributes .= ! empty( $item->url )        ? ' href="'   . esc_url( $item->url        ) .'"' : '';
+		$attributes .= ' class="menu-link ' . ( $depth > 0 ? 'sub-menu-link' : 'main-menu-link' ) . '"';
+
+		$item_output = sprintf( '%1$s<a%2$s>%3$s%4$s%5$s</a>%6$s',
+		    $args->before,
+		    $attributes,
+		    $args->link_before,
+		    apply_filters( 'the_title', $item->title, $item->ID ),
+		    $args->link_after,
+		    $args->after
+		);
+
+		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+
+	}
+
+}
